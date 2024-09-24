5906ea59b789733a13ee7b223d369679642130e9
Franx <fe46315@gmail.com>
2024-09-24 12:41:17 +0200

    Added counter for the score and working on a fix for celeb update.

--- higher_lower.py
+++ higher_lower.py
@@ -11,25 +11,31 @@ def main():
     print("_____________________________________________________________\n"
           "\nWelcome to the classic Higher or Lower game!\n"
           "_____________________________________________________________")
-    celeb_a = random.choice(celebs)
-    celeb_b = random.choice(celebs)
-    # Ensure no two celebrities have been selected.
-    if celeb_a == celeb_b:
-        celeb_b = random.choice(celebs)
-
-    print(f"{celeb_a['name']} VS {celeb_b['name']}!\n"
-          f"\n{celeb_a['name']} has {celeb_a['follower_count']}M followers.\n")
 
 
+    count = 0
     while True:
         try:
+            celeb_a = random.choice(celebs)
+            celeb_b = random.choice(celebs)
+
+            # Ensure no two celebrities have been selected.
+            if celeb_a == celeb_b:
+                celeb_b = random.choice(celebs)
+
+            print(f"{celeb_a['name']} VS {celeb_b['name']}!\n"
+                  f"\n{celeb_a['name']} has {celeb_a['follower_count']}M followers.\n")
+
             user = input(f"Do you think {celeb_b['name']}'s followers is higher or lower than "
                          f"{celeb_a['name']}'s followers?\n(Higher or Lower): ").lower()
 
             if user == "higher":
                 if celeb_b['follower_count'] > celeb_a['follower_count']:
+                    count += 1
                     print(f"Correct! {celeb_b['name']} has {celeb_b['follower_count']}M followers!\n"
-                          f"Which is Higher!")
+                          f"Which is Higher!\n________________________________________________________________"
+                          f"\nPoints : {count}")
+                    celeb_a = celeb_b
                     continue
 
                 else:
@@ -39,8 +45,11 @@ def main():
 
             elif user == "lower":
                 if celeb_b['follower_count'] < celeb_a['follower_count']:
+                    count += 1
                     print(f"Correct! {celeb_b['name']} has {celeb_b['follower_count']}M followers!\n"
-                          f"Which is Lower!")
+                          f"Which is Lower!\n________________________________________________________________"
+                          f"\nPoints : {count}")
+                    celeb_a = celeb_b
                     continue
 
                 else:
